a353b441bd8cb637b680e01e76b391d9351c8ae8
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2025-02-08 18:10:48 +0100

    Added entities and postgres DB

---
+++ src/category/entities/category.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne, OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
+import { User } from "../../user/entities/user.entity";
+import { Transaction } from "../../transaction/entities/transaction.entity";
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn({ name: 'category_id' })
+  id: number;
+
+  @Column()
+  title: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => User, user => user.categories)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transactions: Transaction[];
+
+}
